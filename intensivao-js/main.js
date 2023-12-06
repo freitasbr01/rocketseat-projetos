8b1821b0c75a82cec24a67108c9e3d70fe61462b
freitasbr01 <137903019+freitasbr01@users.noreply.github.com>
2023-12-06 15:23:02 -0300

    Add files via upload

---
+++ intensivao-js/main.js
@@ -0,0 +1,66 @@
+console.log("Hello World!")
+
+let fruit1 = "Banana"
+let fruit2 = "Açai"
+let result = fruit1 + fruit2;
+console.log(result);
+
+
+let age = 30;
+if (typeof age == 'number') {
+  console.log("É um número")
+} 
+else if (typeof age != 'number') {
+  console.log("Não é um número")
+}
+
+
+let fruit = "manga";
+if (typeof fruit == 'string') {
+  console.log('É uma string')
+} else if (typeof fruit != 'string') {
+  console.log('Não é uma string')
+}
+
+
+let isHot = false;
+if (typeof isHot == 'boolean') {
+  console.log('É um booleano!')
+} else if (typeof isHot != 'boolean') {
+  console.log('Não é um booleano!')
+}
+
+
+let n1 = 25
+let n2 = 10
+let sub = n1 - n2 
+console.log(sub)
+
+
+let firstNumber = 10
+let secondNumber = 10
+let mult = firstNumber * secondNumber
+console.log(mult)
+
+
+let boxPen1 = 20
+let boxPen2 = 3
+let div = boxPen1 / boxPen2
+console.log(div)
+
+
+let scoreStudent = 10
+if (scoreStudent % 2 == 0 ) {
+  console.log("É um número par")
+} else {
+  console.log("Não é um número par")
+}
+
+
+let scoreCredit = 9
+if (scoreCredit % 2 != 0) {
+  console.log("É um número ímpar")
+} else {
+  console.log("Não é um número ímpar")
+}
+
